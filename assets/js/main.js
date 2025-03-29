d343a524edb159f5f61d045388ad1af8dabae15d
Yves Talboom <yves.talboom@gmail.com>
2025-03-29 05:54:26 +0100

    Fix build error

---
+++ assets/js/main.js
@@ -0,0 +1,121 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+// import swiper styles bundle
+import "swiper/css";
+import "swiper/css/bundle";
+
+import ScrollReveal from "scrollreveal";
+
+/*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+const navMenu = document.getElementById("nav-menu");
+const navLInk = document.querySelector(".nav-link");
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("left-[0]");
+  hamburger.classList.toggle("ri-close-large-line");
+});
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+
+const scrollUp = function () {
+  const scrollUpBtn = document.getElementById("scroll-up");
+  if (this.scrollY >= 250) {
+    scrollUpBtn.classList.remove("-bottom-1/2");
+    scrollUpBtn.classList.add("bottom-4");
+  } else {
+    scrollUpBtn.classList.add("-bottom-1/2");
+    scrollUpBtn.classList.remove("bottom-4");
+  }
+};
+
+window.addEventListener("scroll", scrollUp);
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+
+const scrollHeader = function () {
+  const header = document.getElementById("navbar");
+  if (this.scrollY >= 50) {
+    header.classList.add("border-b", "border-yellow-500");
+  } else {
+    header.classList.remove("border-b", "border-yellow-500");
+  }
+};
+
+window.addEventListener("scroll", scrollHeader);
+/*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination],
+  // Optional parameters
+  speed: 400,
+  spaceBetween: 30,
+  autoplay: { delay: 3000, disableOnInteraction: false },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  grabCursor: true,
+  breakpoints: {
+    640: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  },
+});
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+const activeLink = function () {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  let current = "home";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (this.scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((item) => {
+    item.classList.remove("active");
+    if (item.href.includes(current)) {
+      item.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", activeLink);
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 300,
+  reset: true,
+});
+
+sr.reveal(
+  `.home__data, .about__top, .popular__top, .review__top, .review__swiper, .footer__icon, .footer__content, .copy__right`,
+);
+sr.reveal(`.home__image`, { delay: 500, scale: 0.5 });
+
+sr.reveal(`.service__card, .popular__card`, { interval: 100 });
+
+sr.reveal(`.about__leaf`, { delay: 1000, origin: "right" });
+sr.reveal(`.about__item_1-content, .about__item_2-img`, {
+  origin: "right",
+});
+
+sr.reveal(`.about__item_2-content, .about__item_1-img`, {
+  origin: "left",
+});
+
+sr.reveal(`.review__leaf, .footer__floral`, {
+  origin: "left",
+  delay: 1000,
+});
